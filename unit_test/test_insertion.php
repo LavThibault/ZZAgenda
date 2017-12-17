ccb48129b76eafd6e764df72a2fdc3e7991eddc5
Géraud François <geraud_francois@orange.fr>
2017-12-17 21:21:32 +0100

    Remove function.php

--- unit_test/test_insertion.php
+++ unit_test/test_insertion.php
@@ -9,7 +9,6 @@
         require_once(__ROOT__.'/functions/file.php');
         require_once(__ROOT__.'/functions/json_parser.php');
         require_once(__ROOT__.'/functions/conf_manager.php');
-        require_once(__ROOT__.'/functions/functions.php');
         require_once(__ROOT__.'/functions/auth.php');
         require_once(__ROOT__.'/functions/csv_parser.php');
         $_POST['titre']="Internet Of Things";
