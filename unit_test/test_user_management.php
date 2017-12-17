38129ee232aa3f5ff86a367b489894a33611b3e7
Géraud François <geraud_francois@orange.fr>
2017-12-17 21:30:32 +0100

    test conversion nombre groupe

---
+++ unit_test/test_user_management.php
@@ -0,0 +1,31 @@
+<?php
+  use PHPUnit\Framework\TestCase;
+
+  class Test extends TestCase {
+
+    public function setUp(){
+        echo "=================================================================== DEBUT DES TESTS";
+        define('__ROOT__', dirname(dirname(__FILE__)));
+        require_once(__ROOT__.'/functions/file.php');
+        require_once(__ROOT__.'/functions/json_parser.php');
+        require_once(__ROOT__.'/functions/conf_manager.php');
+        require_once(__ROOT__.'/functions/auth.php');
+        require_once(__ROOT__.'/functions/csv_parser.php');
+
+    }
+
+    public function conversion_nombre_groupe(){
+      $this -> assertEquals(numberToGroup(1),'user');
+      $this -> assertEquals(numberToGroup(2),'admin');
+      $this -> assertEquals(numberToGroup(-300),'visitor');
+      $this -> assertEquals(groupToNumber('user'),1);
+      $this -> assertEquals(groupToNumber('admin'),2);
+      $this -> assertEquals(groupToNumber('visiazeazerr'),0);
+    }
+
+    public function tearDown(){
+      echo "=================================================================== FIN DES TESTS ";
+    }
+
+  }
+ ?>
